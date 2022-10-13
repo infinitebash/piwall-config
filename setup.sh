c926bb9a86e63cffbd041467f5238f5a56f1e9a4
infinitebash <32340104+infinitebash@users.noreply.github.com>
2022-10-13 14:39:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -56,7 +56,8 @@ sudo dpkg -i pwlibs1_1.1_armhf.deb
 sudo dpkg -i pwomxplayer_20130815_armhf.deb
 
 echo "#!/bin/bash
-pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B" | tee start.sh
+pwomxplayer -A udp://239.0.1.23:1234?buffer_size=1200000B" | tee $HOME/start.sh
+chmod +x $HOME/start.sh
 mkdir -p $HOME/.config/systemd/user
 echo "[Unit]
 Description=Start piwall
